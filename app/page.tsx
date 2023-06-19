6354fb6c935de9ced670abfeff6cb8a59c6f78b0
brodiecc <brodieclarke97@gmail.com>
2023-06-19 13:07:35 +1000

    Added Blog Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Blog from "./components/Blog";
 
 export default function Home() {
   return (
     <main className=" bg-white">
       <Hero />
       <About />
+      <Blog />
     </main>
   );
 }
