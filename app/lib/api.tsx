fb8fc6e30c9b5b060d9b4d96e1786918378deb47
Brodie Clarke <brodieclarke97@gmail.com>
2023-07-10 14:22:27 +1000

    Added optimized api

---
+++ app/lib/api.tsx
@@ -0,0 +1,198 @@
+const API_URL = process.env.WORDPRESS_API_URL;
+
+async function fetchAPI(query = "", { variables }: Record<string, any> = {}) {
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+
+  if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
+    headers[
+      "Authorization"
+    ] = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`;
+  }
+
+  // WPGraphQL Plugin must be enabled
+  if (API_URL) {
+    // Check if API_URL is defined
+    const res = await fetch(API_URL, {
+      headers,
+      method: "POST",
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+
+    const json = await res.json();
+    if (json.errors) {
+      console.error(json.errors);
+      throw new Error("Failed to fetch API");
+    }
+    return json.data;
+  } else {
+    throw new Error("API_URL is not defined");
+  }
+}
+
+// export async function getPreviewPost(id: any, idType = "DATABASE_ID") {
+//   const data = await fetchAPI(
+//     `
+//     query PreviewPost($id: ID!, $idType: PostIdType!) {
+//       post(id: $id, idType: $idType) {
+//         databaseId
+//         slug
+//         status
+//       }
+//     }`,
+//     {
+//       variables: { id, idType },
+//     }
+//   );
+//   return data.post;
+// }
+
+export async function getAllSlugs() {
+  const data = await fetchAPI(`
+    {
+      posts(first: 10000) {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  return data.posts;
+}
+
+// export async function getAllPostsForHome(preview: any) {
+export async function getLatestPosts() {
+  const data = await fetchAPI(
+    `
+    query AllPosts {
+      posts(first: 3, where: {orderby: {field: DATE, order: DESC}}) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+            featuredImage {
+              node {
+                sourceUrl
+              }
+            }
+            categories {
+              nodes {
+                name
+                slug
+              }
+            }
+            author {
+              node {
+                name
+                firstName
+                lastName
+                avatar {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+  );
+
+  return data?.posts.edges;
+}
+
+export async function getPostAndMorePosts(slug: string) {
+  const data = await fetchAPI(
+    `
+    fragment AuthorFields on User {
+      name
+      firstName
+      lastName
+      avatar {
+        url
+      }
+    }
+    fragment PostFields on Post {
+      title
+      excerpt
+      slug
+      date
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      author {
+        node {
+          ...AuthorFields
+        }
+      }
+      categories {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      tags {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+    query PostBySlug($id: ID!, $idType: PostIdType!) {
+      post(id: $id, idType: $idType) {
+        ...PostFields
+        content
+        
+        revisions(first: 1, where: { orderby: { field: MODIFIED, order: DESC } }) {
+          edges {
+            node {
+              title
+              excerpt
+              content
+              author {
+                node {
+                  ...AuthorFields
+                }
+              }
+            }
+          }
+        }
+        
+      }
+      posts(first: 3, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            ...PostFields
+          }
+        }
+      }
+    }
+  `,
+    {
+      variables: {
+        id: slug,
+        idType: "SLUG",
+      },
+    }
+  );
+  // Filter out the main post
+  data.posts.edges = data.posts.edges.filter(
+    ({ node }: { node: { slug: string } }) => node.slug !== slug
+  );
+  // If there are still 3 posts, remove the last one
+  if (data.posts.edges.length > 2) data.posts.edges.pop();
+
+  return data;
+}
