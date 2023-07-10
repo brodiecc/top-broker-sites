9085e910215938460b821e8c13e6961710f678c8
Brodie Clarke <brodieclarke97@gmail.com>
2023-07-10 14:19:12 +1000

    Changed container style

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -8,7 +8,7 @@ type ContainerProps = {
 
 export default function Container({ children, className }: ContainerProps) {
   return (
-    <div className={clsx("mx-auto max-w-[85rem] sm:px-6 lg:px-8", className)}>
+    <div className={clsx(className, "mx-auto max-w-7xl sm:px-6 lg:px-8")}>
       {children}
     </div>
   );
