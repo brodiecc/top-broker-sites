7df0e9165511be73c3177fa5da5fa8d105af64f8
Brodie C <brodieclarke97@gmail.com>
2023-08-02 19:55:52 +0400

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+import Container from "./components/Container";
+import MoreStories from "./components/MoreStories";
+import { getLatestPosts } from "./lib/api";
+
+export default async function NotFound() {
+  const latestPosts = await getPosts();
+
+  return (
+    <div>
+      <Container className="flex justify-center align-middle my-24 flex-col text-center">
+        <h2 className="text-4xl my-8 font-bold">
+          Oops... This page doesn't exist.
+        </h2>
+        <p className="text-lg">Try one of these:</p>
+      </Container>
+
+      <MoreStories posts={latestPosts} />
+    </div>
+  );
+}
+
+async function getPosts() {
+  const latestPosts = await getLatestPosts(3);
+
+  return latestPosts;
+}
