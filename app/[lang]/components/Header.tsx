b5d16e2fcd1f5317f2897e64f0f0dce294e21f9d
Brodie Clarke <brodieclarke97@gmail.com>
2023-08-27 08:17:45 +0400

    Fixed mobile nav headings

--- app/[lang]/components/Header.tsx
+++ app/[lang]/components/Header.tsx
@@ -142,10 +142,10 @@ export default function Header({ lang }: any) {
                 {navigation.map((item) => (
                   <a
                     key={item.name}
-                    href={item.href}
+                    href={`/${lang}${item.href}`}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    {item.name}
+                    {dictionary[item.name]}
                   </a>
                 ))}
               </div>
